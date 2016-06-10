5bcb90daf5687c9f57bce0a6c6d4b222fbc65978
Ángel Faraldo <angelfaraldo@gmail.com>
2016-06-10 20:16:29 +0200

    regular backup

--- midi/m21_load_midi.py
+++ midi/m21_load_midi.py
@@ -1,5 +1,6 @@
 import music21 as m21
 import os
+from math import ceil
 
 
 def load_midi_corpus(midi_corpus_location='/home/angel/Git/house-harmonic-filler/corpus'):
@@ -12,40 +13,37 @@ def load_midi_corpus(midi_corpus_location='/home/angel/Git/house-harmonic-filler
     return midi_corpus_list
 
 
-def load_midfile(file_id, midi_corpus_list=load_midi_corpus()):
+def load_midfile(file_id, midi_corpus_list):
     file_id %= len(midi_corpus_list)
     score = m21.converter.parse(midi_corpus_list[file_id])
     return score[0]
 
-
-corpus = load_midi_corpus()
-raw_loop = load_midfile(33, corpus)
-raw_loop.timeSignature = m21.meter.TimeSignature('4/4')
-raw_loop.makeNotation(inPlace=True)
+# LINYX:
+# corpus = load_midi_corpus()
+# MAC
+corpus = load_midi_corpus('/Users/angel/Git/house-harmonic-filler/corpus')
+raw_loop = load_midfile(22222, corpus)
 clean_loop = m21.stream.Stream()
 last_chord = m21.chord.Chord()
-for c in raw_loop.flat.recurse().getElementsByClass('Chord'):
+for c in raw_loop.recurse().getElementsByClass('Chord'):
+    c.sortAscending(inPlace=True)
+    print c.pitches
     if c.pitches != last_chord.pitches:
-        print c.offset, c.duration
+        print c.offset, c.duration, c.pitches
         clean_loop.insert(c.offset, c.__deepcopy__())
     last_chord = c
-clean_loop.show('text')
-
 
 for i in range(len(clean_loop)):
     print i
     if i < (len(clean_loop) - 1):
-        print 'off', clean_loop[i].offset, clean_loop[i].duration
-        dur = m21.duration.Duration(clean_loop[i + 1].offset - clean_loop[i].offset)
-        print 'dur', dur
-        clean_loop[i].duration = dur
+        clean_loop[i].duration = m21.duration.Duration(clean_loop[i + 1].offset - clean_loop[i].offset)
         print 'off', clean_loop[i].offset, clean_loop[i].duration
     elif i == (len(clean_loop) - 1):
+        clean_loop[i].duration = m21.duration.Duration(ceil(raw_loop.highestTime) - clean_loop[i].offset)
         print 'off', clean_loop[i].offset, clean_loop[i].duration
-        dur = m21.duration.Duration(raw_loop.flat.highestTime - clean_loop[i].offset)
-        # clean_loop[i].duration = m21.duration.Duration(raw_loop.flat.highestTime - clean_loop.offset)
-        clean_loop[i].duration = dur
-        print 'off', clean_loop[i].offset, clean_loop[i].duration
+clean_loop.show()
+
+
 clean_loop.makeNotation(inPlace=True)
 
 clean_loop.show()
