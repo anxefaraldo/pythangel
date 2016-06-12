2ba77b9b74b10af3c5bd299cbe2f9a7cd5c8b16e
Ángel Faraldo <angelfaraldo@gmail.com>
2016-06-12 13:31:57 +0200

    m21_function_defs

--- midi/m21_load_midi.py
+++ midi/m21_load_midi.py
@@ -1,6 +1,7 @@
+from math import ceil
 import music21 as m21
 import os
-from math import ceil
+from sys import platform
 
 
 def load_midi_corpus(midi_corpus_location='/home/angel/Git/house-harmonic-filler/corpus'):
@@ -18,35 +19,44 @@ def load_midfile(file_id, midi_corpus_list):
     score = m21.converter.parse(midi_corpus_list[file_id])
     return score[0]
 
-# LINYX:
-# corpus = load_midi_corpus()
-# MAC
-corpus = load_midi_corpus('/Users/angel/Git/house-harmonic-filler/corpus')
-raw_loop = load_midfile(22222, corpus)
-clean_loop = m21.stream.Stream()
-last_chord = m21.chord.Chord()
-for c in raw_loop.recurse().getElementsByClass('Chord'):
-    c.sortAscending(inPlace=True)
-    print c.pitches
-    if c.pitches != last_chord.pitches:
-        print c.offset, c.duration, c.pitches
-        clean_loop.insert(c.offset, c.__deepcopy__())
-    last_chord = c
 
-for i in range(len(clean_loop)):
-    print i
-    if i < (len(clean_loop) - 1):
-        clean_loop[i].duration = m21.duration.Duration(clean_loop[i + 1].offset - clean_loop[i].offset)
-        print 'off', clean_loop[i].offset, clean_loop[i].duration
-    elif i == (len(clean_loop) - 1):
-        clean_loop[i].duration = m21.duration.Duration(ceil(raw_loop.highestTime) - clean_loop[i].offset)
-        print 'off', clean_loop[i].offset, clean_loop[i].duration
-clean_loop.show()
+def extract_chords(m21_stream):
+    new_stream = m21.stream.Stream()
+    last_chord = m21.chord.Chord()
+    for c in m21_stream.recurse().getElementsByClass('Chord'):
+        c.sortAscending(inPlace=True)
+        print c.pitches
+        if c.pitches != last_chord.pitches:
+            new_stream.insert(c.offset, c.__deepcopy__())
+        last_chord = c
+    for i in range(len(new_stream)):
+        if i < (len(new_stream) - 1):
+            new_stream[i].duration = m21.duration.Duration(new_stream[i + 1].offset - new_stream[i].offset)
+        elif i == (len(new_stream) - 1):
+            new_stream[i].duration = m21.duration.Duration(ceil(m21_stream.highestTime) - new_stream[i].offset)
+    return new_stream
 
 
-clean_loop.makeNotation(inPlace=True)
+def force_4_bar(m21_stream):
+    if m21_stream.highestTime == 8:
+        four_bar_loop = m21.stream.Stream()
+        four_bar_loop.repeatAppend(m21_stream, 2)
+        four_bar_loop.makeNotation(inPlace=True)
+        return four_bar_loop
+    else:
+        return m21_stream
 
-clean_loop.show()
+
+# Actions:
+
+if platform == 'darwin':
+    corpus = load_midi_corpus('/Users/angel/Git/house-harmonic-filler/corpus')
+else:
+    corpus = load_midi_corpus('/home/angel/Git/house-harmonic-filler/corpus')
+
+
+prog = force_4_bar(extract_chords(load_midfile((1, corpus))))
+prog.show()
 
 
 
